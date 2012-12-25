5d4d9242a060a9c11c3ad96ecab5ea66c8575f24
Deepan Rajkumar <deepanrr@thoughtworks.com>
2012-12-25 19:35:09 +0530

    "Adding Tutotials from web collection"

---
+++ MyExp/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+	alert("hi");
+//    $('.size-information input:radio').click(function () {
+//		if($(this).is(':checked')){
+//			$(this).parent().parent().addClass('select');
+//			}
+//	});
+//	$('.size-information input:radio').mousedown(function(){
+//  		var $self = $(this);
+//  		if( $self.is(':checked') ){
+//    var uncheck = function(){
+//      setTimeout(function(){$self.removeAttr('checked');
+//	  $self.parent().parent().removeClass('select');
+//	  },0);
+//    };
+//    var unbind = function(){
+//      $self.unbind('mouseup',up);
+//    };
+//    var up = function(){
+//      uncheck();
+//      unbind();
+//    };
+//    $self.bind('mouseup',up);
+//    $self.one('mouseout', unbind);
+//  }
+//});
+    $('.size-information input:radio').click(function () {
+        if ($(this).is(':checked')) {
+            $(this).parent().parent().addClass('selected');
+            $(this).parent().parent().next().removeClass('selected');
+            $(this).parent().parent().prev().removeClass('selected');
+        }
+    });
+	
+	//Empty Content -- Jquery 
+	
+	$(".empty td:empty").css("background","#0F0").text("This is all Empty")
+	$(".empty").find('td').addClass('hi');
+});
